a316cef7a60d2cbfe78df1fa7f884027d10922e8
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-29 18:28:26 -0300

    Commit29/05 Hice el ejercicio pero con muchos pasos.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,51 @@ let nombresArreglo: string[] = new Array(3);
 let iNombres: number;
 let notasArreglo: number[] = new Array(3);
 let iNotas: number;
+let resultado0: number = 0;
+let resultado1: number = 0;
+let resultado2: number = 0;
 
 for (iNombres = 0; iNombres < nombresArreglo.length; iNombres++) {
-  nombresArreglo[iNombres] = prompt(
-    `Ingrese el nombre del alumno de la posición ${iNombres} es:`
+  nombresArreglo[iNombres] = String(
+    prompt(`Ingrese el nombre del alumno de la posición ${iNombres}`)
+  );
+  console.log(
+    `El nombre del alumno en la posición ${iNombres} es: ${nombresArreglo[iNombres]}`
   );
 }
-for (iNombres = 0; iNombres < nombresArreglo.length; iNombres++) {
-  console.log(`El nombre del alumno en la posición ${iNombres} es:`);
+for (iNotas = 0; iNotas < notasArreglo.length; iNotas++) {
+  notasArreglo[iNotas] = Number(
+    prompt(`Ingrese las notas del alumno ${nombresArreglo[0]}`)
+  );
+  console.log(
+    `Las notas del alumno ${nombresArreglo[0]} son: ${notasArreglo[iNotas]}`
+  );
+  resultado0 += notasArreglo[iNotas] / 3;
 }
+console.log(
+  `El promedio de notas del alumno ${nombresArreglo[0]} es: ${resultado0}`
+);
+for (iNotas = 0; iNotas < notasArreglo.length; iNotas++) {
+  notasArreglo[iNotas] = Number(
+    prompt(`Ingrese las notas del alumno ${nombresArreglo[1]}`)
+  );
+  console.log(
+    `Las notas del alumno ${nombresArreglo[1]} son: ${notasArreglo[iNotas]}`
+  );
+  resultado1 += notasArreglo[iNotas] / 3;
+}
+console.log(
+  `El promedio de notas del alumno ${nombresArreglo[1]} es: ${resultado1}`
+);
+for (iNotas = 0; iNotas < notasArreglo.length; iNotas++) {
+  notasArreglo[iNotas] = Number(
+    prompt(`Ingrese las notas del alumno ${nombresArreglo[2]}`)
+  );
+  console.log(
+    `Las notas del alumno ${nombresArreglo[2]} son: ${notasArreglo[iNotas]}`
+  );
+  resultado2 += notasArreglo[iNotas] / 3;
+}
+console.log(
+  `El promedio de notas del alumno ${nombresArreglo[2]} es: ${resultado2}`
+);
